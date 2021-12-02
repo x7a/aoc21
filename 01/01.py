5bd98bf1b2148debe9eb7230dc8c88de133d75f5
x7a <58111040+x7a@users.noreply.github.com>
2021-12-02 11:30:42 +0100

    Add 01 part 2

--- 01/01.py
+++ 01/01.py
@@ -4,12 +4,27 @@ from sys import stdin
 
 vals = [int(val) for val in stdin.readlines()]
 
-ans = 0
-for i, val in enumerate(vals):
-    if i == 0:
-        continue
-    if val > vals[i - 1]:
-        ans += 1
 
-print(ans)
+def part1():
+    larger = 0
+    for i, val in enumerate(vals):
+        if i == 0:
+            continue
+        if val > vals[i - 1]:
+            larger += 1
+    return larger
 
+
+def part2():
+    larger = 0
+    sums = [val + vals[i + 1] + vals[i + 2] for i, val in enumerate(vals[:-2])]
+    for i, su in enumerate(sums):
+        if i == 0:
+            continue
+        if su > sums[i - 1]:
+            larger += 1
+    return larger
+
+
+print(part1())
+print(part2())
